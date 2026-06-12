c6d0defb06d67544dfdb3cdabebfadca6e6386a6
Redwanul Islam Saikot <shoikotkhan1144@gmail.com>
2026-06-12 23:04:37 +0600

    create

---
+++ src/components/saikot.jsx
@@ -0,0 +1,8 @@
+export default function Saikot() {
+  return (
+    <div className="saikot">
+      <h1>Saikot</h1>
+      <p>Saikot is a fictional character created for demonstration purposes.</p>
+    </div>
+  );
+}
